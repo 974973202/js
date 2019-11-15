73a00bc9db00c0ec6602e67ac91a68a242a2f1d0
DESKTOP-L1UI7PG\pc <2624082953@qq.com>
2019-11-15 18:33:14 +0800

    note

--- Regular/Regular.js
+++ Regular/Regular.js
@@ -4,5 +4,5 @@ const fs = require('fs');
 // const reg = new RegExp('*');
 
 fs.readFile('./Regular.txt', (err, data) => {
-  console.log(data.toString().match(/123/g))
+  console.log(data.toString().match(/(12)3{2}/g))
 })
