14a0b20fd19f668f523744fbd6311e984e74174e
DESKTOP-lzx <2624082953@qq.com>
2019-11-28 11:03:28 +0800

    note

--- Regular/Regular.js
+++ Regular/Regular.js
@@ -1,8 +1,12 @@
-const fs = require('fs');
+// const fs = require('fs');
 
 
-// const reg = new RegExp('*');
+// // const reg = new RegExp('*');
 
-fs.readFile('./Regular.txt', (err, data) => {
-  console.log(data.toString().match(/(12)3{2}/g))
-})
+// fs.readFile('./Regular.txt', (err, data) => {
+//   console.log(data.toString().match(/(12)3{2}/g))
+// })
+
+var regex = /(?<=container)/g
+var string = '<div id="container" class="main"></div>';
+console.log(string.match(regex)); 
---
+++ 小程序canvas.md
@@ -0,0 +1,5 @@
+### canvas生成图片大小问题
+https://juejin.im/post/5d8f97ea5188250bdf5a382c
+https://developers.weixin.qq.com/community/develop/doc/00042e7079cea8cda5488824f5b800
+https://developers.weixin.qq.com/community/develop/doc/00048aa9e181282e68095dcf956800
+https://blog.csdn.net/weixin_34319374/article/details/92504668
